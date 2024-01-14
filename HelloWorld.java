9af2d14fdb81123a8b79c41f2a3dca6df8d837e1
MohdAzghar <mohammedazhar1210@gmail.com>
2024-01-14 20:25:14 +0530

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main (String[]args){
         System.out.println("Hello! Mr.Mohd Azghar pasha");
+        System.out.println("Hello! Mr.Mohd Azeem");
+
     }
 }
